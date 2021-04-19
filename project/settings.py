8933d7953047c8f1349730d5c8afa720788847e4
sba <sba@sba-Vostro-3667.(none)>
2021-04-19 13:57:04 +0100

    data base

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+  # my apps
+  'sinister',
+  'home',
+  'contact',
+  'login',
+  'blog',  
 ]
 
 MIDDLEWARE = [
--- sinister/models.py
+++ sinister/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+Sinister_type = (
+    ('incendie','incendie'),
+    ('accident','accident'),
+)
+
+class Sinister(models.Model):
+    title = models.CharField(max_length=100)
+    sister_type = models.CharField(max_length=20,choices= Sinister_type )
+    description = models.TextField(max_length=500)
+    published_at = models.DateTimeField(auto_now=True)
+  #  category =
+    damage = models.TextField (max_length=500)
+   # location ='''"""
+
+
+    def __str__(self):
+        return self.title
