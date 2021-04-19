c25a8d106ab79c889c27e6f1855cad1e4864b8f4
sba <sba@sba-Vostro-3667.(none)>
2021-04-19 15:18:15 +0100

    urls--views

--- sinister/admin.py
+++ sinister/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Sinister
+from .models import Sinister,Category
 
 admin.site.register(Sinister)
+admin.site.register(Category)
--- sinister/models.py
+++ sinister/models.py
@@ -11,10 +11,16 @@ class Sinister(models.Model):
     sister_type = models.CharField(max_length=20,choices= Sinister_type )
     description = models.TextField(max_length=500)
     published_at = models.DateTimeField(auto_now=True)
-  #  category =
+    category = models.ForeignKey('Category',on_delete=models.CASCADE,null=True)
     damage = models.TextField (max_length=500)
    # location ='''"""
 
 
     def __str__(self):
         return self.title
+
+class Category(models.Model):
+    name = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.name
